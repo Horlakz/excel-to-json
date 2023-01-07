3a7229a5d9888a61e533b39bafa9176cf501637d
Horlakz <officialhorlakz@gmail.com>
2023-01-07 18:15:48 +0100

    init

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const multer = require("multer");
+const axios = require("axios");
+
+const excelToJson = require("./utils");
+
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+const app = express();
+
+app.post("/api/v1/upload", upload.single("file"), async (req, res) => {
+  const { file } = req;
+
+  try {
+    const json = await excelToJson(file.path, [
+      "surname",
+      "firstname",
+      "other",
+      "class",
+      "gender",
+      "parent_name",
+      "parent_email",
+      "parent_number",
+    ]);
+
+    const response = await axios.post(
+      "https://deusa.com.ng/api/store-students",
+      json,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    res.status(200).json({
+      status: "success",
+      message: "File uploaded successfully",
+      data: response.data,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
---
+++ utils.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const xlsx = require("xlsx");
+
+async function excelToJson(inputFile, keys) {
+  // Read the input file as a binary string
+  const data = fs.readFileSync(inputFile, "binary");
+
+  // Parse the binary string into a workbook object
+  const wb = xlsx.read(data, { type: "binary" });
+
+  // Get the first sheet in the workbook
+  const ws = wb.Sheets[wb.SheetNames[0]];
+
+  // Convert the sheet data to a JSON object
+  const jsonData = xlsx.utils.sheet_to_json(ws);
+
+  // Generate the result object using the specified keys
+  const result = {};
+  jsonData.forEach((row, index) => {
+    result[keys[index]] = row;
+  });
+
+  return JSON.stringify(result, null, 2);
+
+  // // Write the JSON object to the output file
+  // fs.writeFileSync(outputFile, JSON.stringify(result, null, 2));
+}
+
+async function main() {
+  // Parse the command line arguments
+  const inputFile = process.argv[2];
+  const outputFile = process.argv[3];
+  const keys = process.argv.slice(4);
+
+  // Validate the arguments
+  if (!inputFile || !outputFile || keys.length === 0) {
+    console.error("Error: Invalid arguments");
+    console.error(
+      "Usage: node excel-to-json.js <input file> <output file> <key1> <key2> ... <keyN>"
+    );
+    process.exit(1);
+  }
+
+  // Convert the Excel file to a JSON file
+  await excelToJson(inputFile, outputFile, keys);
+}
+
+module.exports = excelToJson;
