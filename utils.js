d12169abf3f85894a0d19b23328f50cc35bbf473
Horlakz <officialhorlakz@gmail.com>
2024-07-11 00:01:38 +0100

    refactor: update file upload endpoint and handle Excel file uploads

--- utils.js
+++ utils.js
@@ -1,6 +1,27 @@
 const fs = require("fs");
 const xlsx = require("xlsx");
 
+/**
+ * Formats the keys of an object by converting them to lowercase and replacing spaces with underscores.
+ * @param {Object} obj - The object whose keys need to be formatted.
+ * @returns {Object} - The modified object with formatted keys.
+ */
+function formatKeys(obj) {
+  const keys = Object.keys(obj);
+  const modifiedObj = {};
+  keys.forEach((key) => {
+    const modifiedKey = key.toLowerCase().replace(/\s/g, "_");
+    modifiedObj[modifiedKey] = obj[key];
+  });
+  return modifiedObj;
+}
+
+/**
+ * Converts an Excel file to JSON format.
+ *
+ * @param {string} inputFile - The path to the Excel file.
+ * @returns {Promise<string>} - A Promise that resolves to the JSON representation of the Excel data.
+ */
 async function excelToJson(inputFile) {
   // Read the input file as a binary string
   const data = fs.readFileSync(inputFile, "binary");
@@ -14,18 +35,12 @@ async function excelToJson(inputFile) {
   // Convert the sheet data to a JSON object
   const jsonData = xlsx.utils.sheet_to_json(ws);
 
-  // console.log(jsonData);
+  const result = [];
+  jsonData.forEach((row, _) => {
+    result.push(formatKeys(row));
+  });
 
-  // Generate the result object using the specified keys
-  // const result = {};
-  // jsonData.forEach((row, index) => {
-  //   result[keys[index]] = row;
-  // });
-
-  return JSON.stringify(jsonData, null, 2);
-
-  // // Write the JSON object to the output file
-  // fs.writeFileSync(outputFile, JSON.stringify(jsonData, null, 2));
+  return JSON.stringify(result, null, 2);
 }
 
 module.exports = excelToJson;
