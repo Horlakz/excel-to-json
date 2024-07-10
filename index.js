29156ff246745865e545a1ed10147c848829bae9
Horlakz <officialhorlakz@gmail.com>
2024-07-11 00:01:31 +0100

    refactor: update file upload endpoint and handle Excel file uploads

--- index.js
+++ index.js
@@ -16,37 +16,26 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/api/v1/upload", upload.single("file"), async (req, res) => {
-  // app.post("/api/v1/upload", async (req, res) => {
+app.post("/upload-excel", upload.single("file"), async (req, res) => {
   const { file } = req;
-  const { file_path } = req.body;
+
+  // check if file is an excel file
+  if (!file.originalname.match(/\.(xls|xlsx)$/)) {
+    res.status(400).json({
+      status: "error",
+      message: "Please upload an Excel file",
+    });
+    return;
+  }
 
   try {
-    // if (!file_path) {
-    //   res.status(400).json({ status: "error" });
-    //   return;
-    // }
-
     const json = await excelToJson(file.path);
 
     fs.writeFileSync(file.originalname + ".json", json);
 
-    // console.log(json);
-
-    // const response = await axios.post(
-    //   "https://deusa.com.ng/api/store-students",
-    //   json,
-    //   {
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //   }
-    // );
-
     res.status(200).json({
       status: "success",
       message: "File uploaded and converted successfully",
-      // data: response.data,
       data: json,
     });
   } catch (err) {
